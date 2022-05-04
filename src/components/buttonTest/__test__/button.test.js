92d4b575654cbd8dba84ebafa2e459019cc366aa
ByrdBassPC <byrdbass@gmail.com>
2022-05-04 15:20:25 -0500

    created cleanup for multiple tests of the same component to run, but ran into an error with version 18 not using the render method from react-dom

--- src/components/buttonTest/__test__/button.test.js
+++ src/components/buttonTest/__test__/button.test.js
@@ -2,9 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Button from './../button'
 
-import { render } from '@testing-library/react';
+import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom';
 
+afterEach(cleanup)
 it('renders without crashing', () => {
     // where we write our test!
     const div = document.createElement('div');
@@ -12,6 +13,12 @@ it('renders without crashing', () => {
 })
 
 it('renders button correctly', () => {
-    const {getByTestId} = render(<Button label='click test!'></Button> )
+    const {getByTestId} = render(<Button label='click test!' />)
     expect(getByTestId('button')).toHaveTextContent('click test!')
+})
+
+//TESTING ANOTHER ELEMENT WITH A DIFFERENT LABEL
+it('renders button correctly', () => {
+    const {getByTestId} = render(<Button label='save'/> )
+    expect(getByTestId('button')).toHaveTextContent('save')
 })
