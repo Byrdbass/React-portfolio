5b6d2855ad2b6ddf7448903b9f9295bdf8e34b9b
ByrdBassPC <byrdbass@gmail.com>
2022-05-05 13:50:27 -0500

    made snapshot tests and found a way to update them based off additional snapshots

--- src/components/buttonTest/__test__/button.test.js
+++ src/components/buttonTest/__test__/button.test.js
@@ -5,6 +5,8 @@ import Button from './../button'
 import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom';
 
+import renderer from 'react-test-renderer'
+
 afterEach(cleanup)
 it('renders without crashing', () => {
     // where we write our test!
@@ -21,4 +23,18 @@ it('renders button correctly', () => {
 it('renders button correctly', () => {
     const {getByTestId} = render(<Button label='save'/> )
     expect(getByTestId('button')).toHaveTextContent('save')
+})
+
+it('matches snapshot 1', () =>{
+    const tree = renderer.create(<Button label='save'/>).toJSON()
+    expect(tree).toMatchSnapshot();
+    //FIRST TIME YOU RUN THIS IT WILL CREATE A SNAPSHOT DIR WITH A JSON FILE
+    // when you push the code it will keep track fo the snapshot as well - it will always check to see if there were edits made on the component
+    // we can update the failing snapshot by typing 'u' in the terminal
+
+})
+
+it('matches snapshot 2', () =>{
+    const tree = renderer.create(<Button label='click test!'/>).toJSON()
+    expect(tree).toMatchSnapshot();
 })
